56c4d9b123350b9952480ef9cbd2fcfea61ba984
raisasalsabily <raisa.salsabil@mail.ugm.ac.id>
2023-05-26 10:35:53 +0700

    feat: add revieworder features

---
+++ makanyukfe/src/components/review/DelivItemReview.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function DelivItemReview() {
+  return (
+    <div className="w-full bg-slate-100 flex flex-col gap-1 rounded-md px-4 py-2">
+      <div className="font-semibold">Super cepat</div>
+      <div>15000</div>
+    </div>
+  )
+}
